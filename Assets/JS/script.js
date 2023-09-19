2731367779e5f4cdf1247777afc4ebbccf5099ab
Jgarnaat <jgarnaat8685@yahoo.com>
2023-09-19 18:41:52 -0400

    Update script.js
    adjusted the script for the map and marker creation. cleaned up the script

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,41 +1,66 @@
-// add var for lat lng input
-// add for loop for results
-// change const marker for input variables
-// position = trilat, trilong
-// title = ssid
+//check the results are loaded from the data array
+//check to make sure the markers are loaded properly
+//finish setting up the local storage
+//link the buttons to the index.html files
 // create local storage 
-let map;
 const headers= {'Authorization': 'Basic QUlEZjQ2ZjQ2MmI1ZTZiMjZiYTc3YTFjMjBkM2Y5ZTE5ZmM6ZjIzZGFlMzdlOGQ2OGNlNGQzYmVhMzFmOGExZDk1ZTY='}
-fetch("https://api.wigle.net/api/v2/network/search?onlymine=false&freenet=true&paynet=false&variance=0.02&postalCode=48910&resultsPerPage=10", { headers })
 
-    .then(function (response){
-        return response.json();})
-    .then(function (data) {
-        console.log(data);})
+const recent = [];
+
+var searchBtn = document.querySelector(".searchBtn");
+
+let map;
 
 async function initMap() {
-  const position = { lat: 42.732536, lng: -84.555534 };
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  
+    const position = {lat: 42.68348312,lng: -84.50691223};
+    const { Map } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  map = new Map(document.getElementById("map"), {
-    zoom: 16,
-    center: position,
-    mapId: "Wifi_map",
-  });
+  fetch("https://api.wigle.net/api/v2/network/search?onlymine=false&freenet=true&paynet=false&variance=0.02&postalCode=48910&resultsPerPage=10", { headers })
+  
+    .then(function(response) {
+      return response.json();
+     })
+    .then(function(data){
+      console.log(data.results[0].trilat)
+    
+    for (let i = 0; i < 10; i++) {
+        var ssid= data.results[i].ssid
+        var lat= data.results[i].trilat
+        var lon= data.results[i].trilong
+        var wiLat = parseFloat(lat)
+        var wiLon = parseFloat(lon)
+        
+        console.log(wiLat);
+        console.log(wiLon);
+        
+        const marker = new google.maps.marker.AdvancedMarkerElement({
+          map:map,
+          position: {lat: wiLat,lng: wiLon},
+          title:ssid})
+          window.data=data
+        
+  
+      }})
+
+    
+
+     map = new Map(document.getElementById("map"), {
+      zoom: 16,
+      center: position,
+      mapId: "Wifi_map",
+        })
+      
+      }
+
+function localSave(){
+  recent.unshift(recentSearch);
+    if(recent.length > 5){
+      recent.pop();
+      }
+    }
 
 
-  const marker1 = new AdvancedMarkerElement({
-    map: map,
-    position: { lat: 42.732535, lng: -84.555534 },
-    title: "Uluru",
-  });
-  const marker2 = new AdvancedMarkerElement({
-    map: map,
-    position: { lat: 42.742535, lng: -84.555534 },
-    title: "Uluru",
-  });
-}
-
-
-initMap();
+window.initMap = initMap;
+searchBtn.addEventListener('click',initMap);
