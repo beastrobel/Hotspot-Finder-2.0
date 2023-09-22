faaff4a5686acfb20cea44d7f0dc740c0176e62d
Jgarnaat <jgarnaat8685@yahoo.com>
2023-09-21 20:34:01 -0400

    Update script.js
    created event listener for saved searches, adjusted map to center on search

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -3,10 +3,12 @@
 //make recent searches clickable to recall searches
 const headers = {'Authorization': 'Basic QUlEZjQ2ZjQ2MmI1ZTZiMjZiYTc3YTFjMjBkM2Y5ZTE5ZmM6ZjIzZGFlMzdlOGQ2OGNlNGQzYmVhMzFmOGExZDk1ZTY='}
 const myPosition = {lat: 42.68348312,lng: -84.50691223};
-const zip = document.getElementById("inputOne");
+var zip = document.getElementById("inputOne");
 let recent = [];
 var searchBtn = document.querySelector(".btn-primary");
 var recentSearch = document.getElementById('recent');
+var address = document.getElementById('address');
+var addressResult = [];
 
 let map;
 
@@ -23,10 +25,7 @@ async function initMap() {
 async function search() {
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
   const { Map } = await google.maps.importLibrary("maps")
-  const map = new Map(document.getElementById("map"), {
-    zoom: 8,
-    center: myPosition,
-    mapId: "Wifi_map",});
+ ;
   fetch("https://api.wigle.net/api/v2/network/search?onlymine=false&freenet=true&paynet=false&variance=0.02&postalCode="+zip.value+"&resultsPerPage=10", { headers })
   
 
@@ -39,14 +38,41 @@ async function search() {
     for (let i = 0; i < 10; i++) {
         var lat= data.results[i].trilat
         var lon= data.results[i].trilong
-        var ssid = data.results[i].ssid
+        var wifiName = data.results[i].ssid
+        var state = data.results[i].region
+        var cityName = data.results[i].city
+        var road = data.results[i].road
+        if (houseNumber != null){
+          var houseNumber = data.results[i].housenumber}
+          else{
+            var houseNumber = ''
+          }
+
+        var postalCode = data.results[i].postalcode
         
         new google.maps.Marker({
           map,
           position: {lat: lat,lng: lon},
           icon: "./Assets/images/Favicon.png",
-          title: ssid})
-          window.data=data;          
+          title: wifiName})
+          window.data=data;
+
+        var li = document.createElement('li')
+        var streetAddress = document.createElement('streetAddress')
+        var name = document.createElement('addressName')
+
+        streetAddress.innerHTML = houseNumber + ' ' + road + ',' + cityName + ',' + state + ' ' + postalCode;
+        name.innerHTML = wifiName
+        
+        li.appendChild(streetAddress);
+        li.appendChild(name);
+
+        address.appendChild(li);
+           
+        const map = new Map(document.getElementById("map"), {
+          zoom: 8,
+          center: {lat: data.results[0].trilat,lng: data.results[0].trilong},
+          mapId: "Wifi_map",})
       }
 
       localSave()
@@ -55,16 +81,22 @@ async function search() {
 
 function localLoad(){
   recent = JSON.parse(localStorage.getItem("recent"))
-
+  
+  
   for (let i = 0; i < recent.length; i++) {
-    var li = document.createElement('li');
+    var li = document.createElement('a');
     li.className = 'search';
 
     var postal = document.createElement('postalCode');
     postal.innerHTML = recent[i];
+    postal.className =('list-group-item list-group-item-action'); 
 
     li.appendChild(postal);
     recentSearch.appendChild(li); 
+
+    postal.addEventListener('click', search)
+    zip = recent[i];
+
   }
 }
 
